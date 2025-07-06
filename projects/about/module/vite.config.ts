c06e7e200190ce7047cff919c8974276b37de370
Marek Brieger <m.brieger@appcited.com>
2025-07-06 11:54:01 +0000

    chore: add @weeker/about-module

---
+++ projects/about/module/vite.config.ts
@@ -0,0 +1,32 @@
+import vue from "@vitejs/plugin-vue"
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite"
+
+import packageJson from "./package.json"
+
+export default defineConfig({
+  build: {
+    // better HMR support
+    emptyOutDir: false,
+    target: "esnext",
+    minify: false,
+    lib: {
+      entry: "src",
+      cssFileName: "style",
+    },
+    rollupOptions: {
+      external: Object.keys(packageJson.peerDependencies).map(lib => new RegExp(`^${lib}`)),
+      output: [
+        {
+          entryFileNames: "[name].js",
+          preserveModules: true,
+          preserveModulesRoot: "src",
+        },
+      ],
+    },
+  },
+  plugins: [
+    vue(),
+    vueDevTools()
+  ],
+})
