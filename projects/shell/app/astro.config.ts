74c0178e51bcc54fc4518cfdb0a11daebb0cc1ab
Marek Brieger <m.brieger@appcited.com>
2025-07-06 15:42:41 +0000

    chore: add pack and publish targets ans update ci workflow to publish shell-app

--- projects/shell/app/astro.config.ts
+++ projects/shell/app/astro.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import node from "@astrojs/node"
 
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue()],
+  output: "server",
+  server: {
+    host: true,
+  },
+  adapter: node({
+    // TODO: use middleware mode
+    mode: "standalone",
+  }),
 });
