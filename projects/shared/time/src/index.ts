817bcb534cb55c4fb7c41d79f692ebd9bd000d97
Marek Brieger <m.brieger@appcited.com>
2025-07-27 14:34:16 +0000

    refactor: move timestampIntervalString

--- projects/shared/time/src/index.ts
+++ projects/shared/time/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./day";
+export * from "./interval";
 export * from "./plain-date-time";
 export * from "./week";
 export * from "./zoned-date-time";
--- projects/shared/time/src/zoned-date-time/index.ts
+++ projects/shared/time/src/zoned-date-time/index.ts
@@ -1,3 +1,2 @@
 export * from "./Rfc9557ZonedDateTime.ts";
 export * from "./zonedDateTime.ts";
-export * from "./timestampIntervalString.ts";
