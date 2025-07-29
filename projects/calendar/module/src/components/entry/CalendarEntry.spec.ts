23f3f219e978cd98e6a774a9bed95d28fbc14af7
Marek Brieger <m.brieger@appcited.com>
2025-07-29 20:39:07 +0000

    refactor(calendar-module): add component CalendarEntry

---
+++ projects/calendar/module/src/components/entry/CalendarEntry.spec.ts
@@ -0,0 +1,38 @@
+import { render } from 'vitest-browser-vue'
+import CalendarEntry from './CalendarEntry.vue'
+import { expect, it } from 'vitest'
+import { zonedDateTime } from '@weeker/shared-time'
+
+type RenderOptions = Parameters<typeof render<typeof CalendarEntry>>[1]
+const renderOptions: RenderOptions = {
+  props: {
+    entry: {
+      id: "9ed12927-7e76-4866-ab1b-f0698ca1312f",
+      name: "Entry Name",
+      start: zonedDateTime("2024-09-19T09:00:00[Europe/Berlin]"),
+      end: zonedDateTime("2024-09-19T19:00:00[Europe/Berlin]"),
+      color: "#ff0000",
+    },
+    style: {
+      height: "400px",
+      width: "200px",
+      margin: "auto",
+    },
+  },
+}
+
+it("displays entry name", async () => {
+  const component = render(CalendarEntry, renderOptions)
+
+  await expect
+    .element(component.getByText("Entry Name"))
+    .toBeVisible()
+})
+
+it('renders entry colors', async ({ task }) => {
+  const component = render(CalendarEntry, renderOptions)
+
+  await expect
+    .element(component.container.children[0])
+    .toMatchScreenshot(task.name)
+})
---
+++ projects/calendar/module/src/components/entry/CalendarEntry.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { Entry } from '#src/model/Entry.ts';
+
+const { entry } = defineProps<{
+  entry: Entry
+}>()
+</script>
+
+<template>
+  <div class="calendar-entry" :style="{
+    '--background-color': entry.color,
+  }">
+    {{ entry.name }}
+  </div>
+</template>
+
+<style lang="css" scoped>
+.calendar-entry {
+  --border-color: color-mix(in srgb, var(--background-color), #000000 20%);
+  --text-color: #ffffff;
+  --border-radius: 0.25rem;
+
+  background-color: var(--background-color);
+  color: var(--text-color);
+  border: 1px solid var(--border-color);
+  border-radius: var(--border-radius);
+  padding: 0.25rem;
+  user-select: none;
+}
+</style>
