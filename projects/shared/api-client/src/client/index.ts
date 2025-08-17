1031abf1e30e68a7a85a8777de2c02044c6d762c
Marek Brieger <m.brieger@appcited.com>
2025-08-17 10:19:43 +0000

    refactor: simplify Client type definition in getClient function

--- projects/shared/api-client/src/client/index.ts
+++ projects/shared/api-client/src/client/index.ts
@@ -5,7 +5,9 @@ import { OpenAPILink } from '@orpc/openapi-client/fetch'
 
 import { contract } from '@weeker/shared-api';
 
-export const getClient: ({ baseUrl }: { baseUrl: string }) => JsonifiedClient<ContractRouterClient<typeof contract>> = ({ baseUrl }) => {
+export type Client = JsonifiedClient<ContractRouterClient<typeof contract>>;
+
+export const getClient: ({ baseUrl }: { baseUrl: string }) => Client = ({ baseUrl }) => {
     const link = new OpenAPILink(contract, {
         url: baseUrl,
     })
