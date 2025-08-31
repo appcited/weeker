30fa004afb545a036caca37276714a5e39cf690b
Marek Brieger <m.brieger@appcited.com>
2025-08-31 13:33:24 +0000

    refactor: change mock implementation of EntriesListHandler

--- projects/calendar/backend/src/index.ts
+++ projects/calendar/backend/src/index.ts
@@ -1,6 +1,6 @@
 import { type Implementer } from "@orpc/server"
 import { contract } from "@weeker/shared-api"
-import { instant } from "@weeker/shared-time"
+import { now } from "@weeker/shared-time"
 
 type ContractImplementer = Implementer<typeof contract, Record<never, never>, Record<never, never>>
 type EntriesListHandler = Parameters<ContractImplementer["entries"]["list"]["handler"]>[0]
@@ -11,8 +11,8 @@ export const entriesListHandler: EntriesListHandler = async ({ }) => {
       id: '123e4567-e89b-12d3-a456-426614174000',
       name: 'Entry 3',
       color: '#ff0000',
-      start: instant('2025-08-17T12:00:00Z').epochMilliseconds,
-      end: instant('2025-08-17T18:00:00Z').epochMilliseconds,
+      start: now().toZonedDateTimeISO("Europe/Berlin").withPlainTime("12:00").epochMilliseconds,
+      end: now().toZonedDateTimeISO("Europe/Berlin").withPlainTime("18:00").epochMilliseconds,
     },
   ]
 }
